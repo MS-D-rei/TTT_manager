05061b92b371e9385baa02ab413770c5029a420c
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2022-02-14 00:18:16 +0900

    add name column to users

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -4,8 +4,23 @@ class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
+  # def new
+  #   self.resource = resource_class.new(sign_in_params)
+  #   clean_up_passwords(resource)
+  #   yield resource if block_given?
+  #   respond_with(resource, serialize_options(resource))
+  # end
 
   # POST /resource/sign_in
+  # def create
+  #   self.resource = warden.authenticate!(auth_options)
+  #   set_flash_message!(:notice, :signed_in)
+  #   sign_in(resource_name, resource)
+  #   yield resource if block_given?
+  #   respond_with resource, location: after_sign_in_path_for(resource)
+  # end
+
+  # will change this method
   def create
     @user = User.find_by(email: params[:user][:email].downcase)
     if @user&.valid_password?(params[:user][:password])
