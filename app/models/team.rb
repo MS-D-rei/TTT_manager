b6e4408523a51fbfcccd6275dd3e55f309b5b4b2
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2022-02-17 15:43:42 +0900

    add has_many setting to users and teams model

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,6 +1,8 @@
 class Team < ApplicationRecord
-  has_many :topics
+  has_many :topics, dependent: :destroy
   belongs_to :leader, class_name: 'User', foreign_key: :leader_id
+  has_many :assigns, dependent: :destroy
+  has_many :members, through: :assigns, source: :user
 
   validates :name, presence: true, length: { maximum: 50 }
   validates :leader_id, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :topics
+  has_many :assigns, dependent: :destroy
+  has_many :teams, through: :assigns
 
   validates :name, presence: true, length: { maximum: 50 }
 end
