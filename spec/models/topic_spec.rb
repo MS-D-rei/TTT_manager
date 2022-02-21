ce69c499e8026f714f2d7290bb6c3d900e8033a2
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2022-02-21 18:38:32 +0900

    add user_id, team_id test to topic test and erase FactoryBot word

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -1,19 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Topic, type: :model do
-  let!(:first_user) { FactoryBot.create(:user) }
-  let!(:first_team) { FactoryBot.create(:team, leader_id: first_user.id) }
+  let!(:user) { create(:user) }
+  let!(:team) { create(:team, leader_id: user.id) }
 
   describe '#validates' do
     context 'when input parameters properly' do
-      let!(:proper_topic) { FactoryBot.build(:topic, user_id: first_user.id, team_id: first_team.id) }
+      let!(:proper_topic) { build(:topic, user_id: user.id, team_id: team.id) }
       it 'should be true' do
         expect(proper_topic.save).to be_truthy
       end
     end
 
     context 'when no title' do
-      let!(:no_title_topic) { FactoryBot.build(:topic, title: '', user_id: first_user.id, team_id: first_team.id) }
+      let!(:no_title_topic) { build(:topic, title: '', user_id: user.id, team_id: team.id) }
       it 'should be false' do
         expect(no_title_topic.save).to be_falsy
       end
@@ -22,8 +22,7 @@ RSpec.describe Topic, type: :model do
     context 'when long title over 200 chars' do
       let!(:too_long_title) { ('a' * 201).to_s }
       let!(:long_title_topic) do
-        FactoryBot.build(:topic, title: too_long_title,
-                                 user_id: first_user.id, team_id: first_team.id)
+        build(:topic, title: too_long_title, user_id: user.id, team_id: team.id)
       end
       it 'should be false' do
         expect(long_title_topic.save).to be_falsy
@@ -32,7 +31,7 @@ RSpec.describe Topic, type: :model do
 
     context 'when no description' do
       let!(:no_description_topic) do
-        FactoryBot.build(:topic, description: '', user_id: first_user.id, team_id: first_team.id)
+        build(:topic, description: '', user_id: user.id, team_id: team.id)
       end
       it 'should be false' do
         expect(no_description_topic.save).to be_falsy
@@ -42,7 +41,7 @@ RSpec.describe Topic, type: :model do
     context 'when too long description over 1,000 chars' do
       let!(:too_long_description) { ('a' * 1001).to_s }
       let!(:long_descripiton_topic) do
-        FactoryBot.build(:topic, description: too_long_description, user_id: first_user.id, team_id: first_team.id)
+        build(:topic, description: too_long_description, user_id: user.id, team_id: team.id)
       end
       it 'should be false' do
         expect(long_descripiton_topic.save).to be_falsy
@@ -51,7 +50,7 @@ RSpec.describe Topic, type: :model do
 
     context 'when no priority' do
       let!(:no_priority_topic) do
-        FactoryBot.build(:topic, priority: '', user_id: first_user.id, team_id: first_team.id)
+        build(:topic, priority: '', user_id: user.id, team_id: team.id)
       end
       it 'should be false' do
         expect(no_priority_topic.save).to be_falsy
@@ -60,7 +59,7 @@ RSpec.describe Topic, type: :model do
 
     context 'when no deadline' do
       let!(:no_deadline_topic) do
-        FactoryBot.build(:topic, deadline: '', user_id: first_user.id, team_id: first_team.id)
+        build(:topic, deadline: '', user_id: user.id, team_id: team.id)
       end
       it 'should be false' do
         expect(no_deadline_topic.save).to be_falsy
@@ -69,10 +68,24 @@ RSpec.describe Topic, type: :model do
 
     context 'when no status' do
       let!(:no_status_topic) do
-        FactoryBot.build(:topic, status: '', user_id: first_user.id, team_id: first_team.id)
+        build(:topic, status: '', user_id: user.id, team_id: team.id)
       end
       it 'should be false' do
         expect(no_status_topic.save).to be_falsy
+      end
+    end
+
+    context 'when no user_id' do
+      let!(:no_user_id_topic) { build(:topic, team_id: team.id) }
+      it 'should be false' do
+        expect(no_user_id_topic.save).to be_falsy
+      end
+    end
+
+    context 'when no team_id' do
+      let!(:no_team_id_topic) { build(:topic, user_id: user.id) }
+      it 'should be false' do
+        expect(no_team_id_topic.save).to be_falsy
       end
     end
   end
