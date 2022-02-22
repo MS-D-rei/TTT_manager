aa623b640e6d7d97f6cc64349fd97646e827ec07
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2022-02-22 16:02:29 +0900

    modify alert message when normal user access admin pages

--- config/initializers/rails_admin_cancan.rb
+++ config/initializers/rails_admin_cancan.rb
@@ -3,7 +3,7 @@ require 'rails_admin/main_controller'
 module RailsAdmin
   class MainController < RailsAdmin::ApplicationController
     rescue_from CanCan::AccessDenied do |exception|
-      flash[:alert] = 'Access denied.'
+      flash[:danger] = 'Access denied, only admin user can access admin pages'
       redirect_to main_app.root_path
     end
   end
