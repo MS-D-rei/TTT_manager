06e9aa3b55339ff353ff30a0b06bd8f568b4e2aa
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2022-02-23 22:38:11 +0900

    add before do inside describe in assign system spec

--- spec/system/assigns_spec.rb
+++ spec/system/assigns_spec.rb
@@ -6,12 +6,14 @@ RSpec.describe 'Assigns', type: :system do
   let!(:team) { create(:team, name: 'First Team', leader_id: normal_user.id) }
 
   describe '#create' do
+    before do
+      visit new_user_session_path
+      normal_user_log_in
+      click_on 'チーム一覧'
+      click_on 'First Team'
+    end
     context 'user invite other user to a team' do
       it 'other user will be a member' do
-        visit new_user_session_path
-        normal_user_log_in
-        click_on 'チーム一覧'
-        click_on 'First Team'
         find('#assign_user_id').find("option[value='#{admin_user.id}']").select_option
         expect { click_on '招待' }.to change { team.members.count }.by(1)
       end
@@ -19,10 +21,6 @@ RSpec.describe 'Assigns', type: :system do
 
     context "user didn't select any user to invite" do
       it 'raise a notification, please select member to invite' do
-        visit new_user_session_path
-        normal_user_log_in
-        click_on 'チーム一覧'
-        click_on 'First Team'
         click_on '招待'
         expect(page).to have_content '招待するメンバーを選択してください'
       end
