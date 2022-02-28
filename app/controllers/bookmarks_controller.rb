11febea0451a5c19378770f700160ead86e1b22e
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2022-02-28 14:10:22 +0900

    add ajax bookmark create and destroy

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -7,13 +7,25 @@ class BookmarksController < ApplicationController
 
   def create
     @task = Task.find(params[:task_id])
-    current_user.bookmark(@task)
-    redirect_to request.referer
+    @bookmark = @task.bookmarks.new(user_id: current_user.id)
+    respond_to do |format|
+      if @bookmark.save
+        format.js { render :create }
+      else
+        format.html { redirect_to request.referer }
+      end
+    end
+    # current_user.bookmark(@task)
   end
 
   def destroy
     @task = current_user.bookmarks.find(params[:id]).task
-    current_user.unbookmark(@task)
-    redirect_to request.referer
+    @bookmark = @task.bookmarks.find_by(user_id: current_user.id)
+    respond_to do |format|
+      return format.html { redirect_to request.referer } if @bookmark.nil?
+
+      @bookmark.destroy
+      format.js { render :destroy }
+    end
   end
 end
